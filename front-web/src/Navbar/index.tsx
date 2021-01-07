2aef16af9024b7d24b66dd0ba41ce0b02a40c82b
RafaelRNG <rafael.gomila@hotmail.com>
2021-01-06 23:17:15 -0300

    Add Home page

--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -2,7 +2,7 @@ import "./styles.css";
 import { ReactComponent as Logo } from "./logo.svg";
 
 function Navbar() {
-    //56:22
+    //1:16:15
     return (
         <nav className="main-navbar">
             <Logo />
