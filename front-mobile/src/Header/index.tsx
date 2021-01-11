02c019ed23e1aae672f9667913cc6124ea016ca6
RafaelRNG <rafael.gomila@hotmail.com>
2021-01-10 23:35:33 -0300

    Add orders list

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 import { View, Text, StyleSheet, Image } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
-export default function Header() {
+function Header() {
+
+    const navigation = useNavigation();
+
+    const handleOnPress = () => {
+        navigation.navigate("Home");
+    }
+
     return (
-        <View style={styles.container}>
-            <Image source={require("../assets/logo.png")} />
-            <Text style={styles.text}>DS Delivery</Text>
-        </View>
+        <TouchableWithoutFeedback onPress={handleOnPress}>
+            <View style={styles.container}>
+                <Image source={require("../assets/logo.png")} />
+                <Text style={styles.text}>DS Delivery</Text>
+            </View>
+        </TouchableWithoutFeedback>
     )
 }
 
@@ -28,3 +39,5 @@ const styles = StyleSheet.create({
         fontFamily: "OpenSans_700Bold"
     }
 });
+
+export default Header;
